26acacd938cb9a435b0918b8aade7a0086cd134d
Adrian Law <alaw15@mail.ccsf.edu>
2019-10-21 09:34:26 -0700

    Refactored code and added header for function prototypes

--- parallelFor.cpp
+++ parallelFor.cpp
@@ -1,42 +1,63 @@
-#include<iostream>
-#include<omp.h>
-#include<chrono>
+#include <iostream>
+#include <omp.h>
+#include <chrono>
+
+#include "proto-demos.hpp"
 
 using namespace std::chrono;
 
-long parallelFor() {
+long parallelFor()
+{
+
 	auto start = high_resolution_clock::now();
+
 	#pragma omp parallel for schedule(static, 10000)
 	for (unsigned i = 0; i < 5000000; ++i) {
 	}
+
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<nanoseconds>(stop - start);
-  	long time = duration.count();
-  	return time;
+	long time = duration.count();
+
+	return time;
+
 }
 
-long nonParallelFor() {
+long nonParallelFor()
+{
+
 	auto start = high_resolution_clock::now();
+
 	for (unsigned i = 0; i < 5000000; ++i) {
 	}
+
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<nanoseconds>(stop - start);
-  	long time = duration.count();
-  	return time;
+	long time = duration.count();
+
+	return time;
 }
 
-int main() {
-	
-  long b = nonParallelFor();
-  long a = parallelFor();
+int main()
+{
 
- std::cout<<"parallelFor(): " << a <<"\nnonParallelFor(): " << b <<std::endl;
+	long b = nonParallelFor();
+	long a = parallelFor();
 
-  if(a<b) {
-    std::cout <<"parallelFor() successful!"<<std::endl;
-  } else {
-    std::cout <<"for() failed!"<<std::endl;
-  }
+	std::cout <<"parallelFor(): " << a
+		  <<"\nnonParallelFor(): " << b
+		  <<std::endl;
+
+	if(a<b) {
+
+		std::cout << "parallelFor() successful!" << std::endl;
+
+	}
+	else {
+
+		std::cout << "for() failed!" << std::endl;
+
+	}
 
 	return 0;
 }
---
+++ proto-demos.hpp
@@ -0,0 +1,14 @@
+#ifndef __DEMO_PROTOS
+#define __DEMO_PROTOS
+
+#include <vector>
+
+// def ===parallelSearch.cpp===
+long sequentialSearch(std::vector<int>, int);
+long parallelSequentialSearch(std::vector<int>, int);
+
+// def ===parallelFor.cpp===
+long parallelFor();
+long nonParallelFor();
+
+#endif /* __DEMO_PROTOS */
